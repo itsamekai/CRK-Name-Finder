30925c62c3fb8d5aa9acd05efe116f44aa5012dc
itsamekai <resayz4@ymail.com>
2022-02-19 21:47:20 +0800

    lowered timings per search

--- main.py
+++ main.py
@@ -2,35 +2,39 @@ import keyboard as kb
 import time
 import pyautogui as pg
 
+
+# this is for my own use. you can run coordinates.py to determine the coordinates respectively.
+
+# ------------ first monitor!! --------------
 # taken: (56, 167, 231)
 # not taken: (255, 255, 255)
 # search fullscreen: x=858, y=88
 # result fullscreen: x = 1111, y = 292
 # ok fullscreen: x=1750, y=978
 
-# array to store all words that are not available and unavailable
+# ----------- second monitor ---------------
+# search fullscreen: x=2860, y=106
+# result fullscreen: x=3037, y=294
+
+# array to store all words that are not available
 available = []
 availIndex = []
-notAvail = []
-
 
 # can change coordinates whenever
 def doAction(name, resultX, resultY):
     # pg.click(searchX, searchY)
-    time.sleep(0.1)
+    time.sleep(0.15)
     # delete the written words
-    kb.send('ctrl+a, delete')
-    time.sleep(0.1)
+    kb.send('ctrl+a')
+    time.sleep(0.2)
+    kb.send('delete')
+    time.sleep(0.15)
     # write new word
-    # for some reason .write enters? so i added an extra.
+    # for some reason .write enters?
     kb.write(name, delay=0.05)
-    time.sleep(0.05)
-    kb.send('enter')
-    time.sleep(0.1)
-    kb.send('enter')
-    time.sleep(0.1)
+    time.sleep(0.15) 
     rgb = pg.pixel(resultX, resultY)
-    time.sleep(0.1)
+    time.sleep(0.15)
     kb.send('enter')
     print("RGB: " + str(rgb))
     return rgb
@@ -40,20 +44,19 @@ def doAction(name, resultX, resultY):
 
 count = 1
 
-with open('names.txt', 'r') as n:
+start = time.time()
+with open('filtered.txt', 'r') as n:
     print("first click")
-    pg.click(858, 88)
+    pg.click(2860, 106)
     time.sleep(0.5)
     for name in n:
-        rgb = doAction(name, 1111, 292)
+        rgb = doAction(name, 3037, 294)
         # we compare by rgb. assume that we can just make use of the color values to determine
         if (rgb == (56, 167, 231)):
-            print(name.strip() + " is not available!\n")
-            notAvail.append(name)
-        else:
-            print(name.strip() + " is available!\n")
+            print(str(count) + ": " + name.strip() + " is not available!\n")
+        else:   
+            print(str(count) + ": " + name.strip() + " is available!\n")
             available.append(name)
-            availIndex.append(count)
         count+= 1
 
 
@@ -63,9 +66,8 @@ for names in available:
     availFile.write(names)
 
 
-print("Index: " + availIndex)
-print("count: " + count)
-
+print("count: " + str(count))
+print("%s seconds taken" % (time.time() - start))
 
 
 
